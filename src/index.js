5fccd3e51752ab11d35af6a974a966f11e26a10a
Siddharth <onepiece@Siddharths-MacBook-Air.local>
2025-12-01 14:41:46 +0530

    disable dotenv in production

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const TOKEN = process.env.BOT_TOKEN;
 
